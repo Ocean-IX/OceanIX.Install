9066cdae54eb331d8fd4a84041e5d05c7a943723
RouteIX <77181250+RouteIX@users.noreply.github.com>
2021-01-13 17:28:45 +1030

    Update and rename install.debian.sh to install.debian.v6NAT.sh

--- install.debian.v6NAT.sh
+++ install.debian.v6NAT.sh
@@ -76,6 +76,16 @@ cp -rlf /tmp/OceanIXP/* /
 rm -rf /tmp/OceanIXP
 
 git clone http://github.com/Ocean-IX/OceanIX.Control.git /opt/oceanixp/www
+NEW_V6=$(cat /dev/urandom | tr -dc 'a-f1-9' | fold -w 4 | head -n 1)
+echo "Setting up IPv6 Connectivity"
+sudo tee /etc/docker/daemon.json > /dev/null << EOF
+{
+  "ipv6": true,
+  "fixed-cidr-v6": "$NEW_V6"
+}
+EOF
+
+sudo systemctl restart docker
 
 IP_ADDR=$(curl -s https://api.ipify.org)
 clear
